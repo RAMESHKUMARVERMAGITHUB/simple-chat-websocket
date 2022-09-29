0cf36a5d87f364acd28545d007deb04c793cd012
Christian Troy Andrada <christian.adrada@collabera.com>
2022-09-29 10:58:40 +0800

    added listener for both html and websocket

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const WebSocket = require('ws');
-const port = 8080;
+const port = 6969;
+const port2 = 8080;
+const app = express();
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server });
 
@@ -14,6 +17,14 @@ wss.on('connection', (ws) => {
         })    
     })
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'/index.html'))
+})
+app.listen(port2, () => {
+    console.log('html server running on port ' + port2)
+})
+
 
 server.listen(port, () => {
     console.log(`Server is listening port ${port}`)
