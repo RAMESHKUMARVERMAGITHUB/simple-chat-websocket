023682df7da36aee725055d07fae506f8f40d32f
Christian Troy Andrada <christian.adrada@collabera.com>
2022-09-30 13:23:39 +0800

    remove the webpage, this will be purely ws server

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const http = require('http');
-const path = require('path');
 const WebSocket = require('ws');
 const port = 6969;
-const port2 = 8080;
-const app = express();
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server });
 
@@ -17,14 +14,6 @@ wss.on('connection', (ws) => {
         })    
     })
 })
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/index.html'))
-})
-app.listen(port2, () => {
-    console.log('html server running on port ' + port2)
-})
-
 
 server.listen(port, () => {
     console.log(`Server is listening port ${port}`)
